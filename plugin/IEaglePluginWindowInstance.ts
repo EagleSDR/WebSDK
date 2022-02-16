5696e4b66d495169081b35b4356141cc4b8ebe9d
Roman-Port <rvporterfield@gmail.com>
2022-02-15 22:09:35 -0600

    Replaced plugin icon with a canvas instead of the jankiness of passing an element around.

--- plugin/IEaglePluginWindowInstance.ts
+++ plugin/IEaglePluginWindowInstance.ts
@@ -7,8 +7,8 @@ export default interface IEaglePluginWindowInstance {
     // Defines the name of the group. Items with the same group name are automatically grouped.
     GetGroupName(): string;
 
-    // Generates a preview element. Should not be large or take a long time to create.
-    GetPreview(): HTMLElement;
+    // Generates a preview element. The user is expected to draw to this canvas.
+    GetPreview(canvas: CanvasRenderingContext2D, width: number, height: number): void;
 
     // Gets the default settings to create the window with.
     GetSettings(): any;
